d2e65da12790b85973f9816e45f76adf8e5d65cd
ki8vi <oneofbln@gmail.com>
2023-03-27 22:29:04 +0600

    map file?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./index.scss"
 function App() {
   return (
     <div className="wrapper">
-      <Drawer title = "samsung"/>
+      {/* <Drawer /> */}
       <Header />
       <Content/>
     </div>
